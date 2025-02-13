7a9aefdabb73f07f4fc1d82e29890c5315a04b85
ppg0001 <ppg0001@auburn.edu>
2025-02-13 13:37:54 -0600

    commit

---
+++ CodingChallenge2_IntroDataVis.R
@@ -0,0 +1,9 @@
+### CodingChallenge2_IntroDataVis ####
+## Author: Pankaj
+
+
+# Setting up relative path to the file for analysis
+csv <- read.csv("C:/Users/panka/OneDrive/Documents/PLPA_Assignment/MycotoxinData.csv", na.strings = "na")
+
+
+
